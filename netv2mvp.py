6980707ceef9e25ed583b51a32761762f35fa6eb
Florent Kermarrec <florent@enjoy-digital.fr>
2017-12-30 14:23:50 +0100

    netv2mvp: add pcie (x1, x2 and x4)

--- netv2mvp.py
+++ netv2mvp.py
@@ -62,6 +62,36 @@ _io = [
         Subsignal("cs_n", Pins("V9"), IOStandard("SSTL15")),
         Misc("SLEW=FAST"),
     ),
+
+    ("pcie_x1", 0,
+        Subsignal("rst_n", Pins("E18"), IOStandard("LVCMOS33")),
+        Subsignal("clk_p", Pins("F10")),
+        Subsignal("clk_n", Pins("E10")),
+        Subsignal("rx_p", Pins("D11")),
+        Subsignal("rx_n", Pins("C11")),
+        Subsignal("tx_p", Pins("D5")),
+        Subsignal("tx_n", Pins("C5"))
+    ),
+
+    ("pcie_x2", 0,
+        Subsignal("rst_n", Pins("E18"), IOStandard("LVCMOS33")),
+        Subsignal("clk_p", Pins("F10")),
+        Subsignal("clk_n", Pins("E10")),
+        Subsignal("rx_p", Pins("D11 B10")),
+        Subsignal("rx_n", Pins("C11 A10")),
+        Subsignal("tx_p", Pins("D5 B6")),
+        Subsignal("tx_n", Pins("C5 A6"))
+    ),
+
+    ("pcie_x4", 0,
+        Subsignal("rst_n", Pins("E18"), IOStandard("LVCMOS33")),
+        Subsignal("clk_p", Pins("F10")),
+        Subsignal("clk_n", Pins("E10")),
+        Subsignal("rx_p", Pins("D11 B10 D9 B8")),
+        Subsignal("rx_n", Pins("C11 A10 C9 A8")),
+        Subsignal("tx_p", Pins("D5 B6 D7 B4")),
+        Subsignal("tx_n", Pins("C5 A6 C7 A4"))
+    ),
 ]
 
 
@@ -226,13 +256,68 @@ class BaseSoC(SoCSDRAM):
         self.comb += self.sys_led.eq(sys_counter[26])
 
 
+class PCIeSoC(BaseSoC):
+    csr_map = {
+        "pcie_phy": 20,
+        "dma":      21,
+        "msi":      22,
+    }
+    csr_map.update(BaseSoC.csr_map)
+
+    interrupt_map = {
+        "dma_writer": 0,
+        "dma_reader": 1,
+    }
+    interrupt_map.update(BaseSoC.interrupt_map)
+
+    BaseSoC.mem_map["csr"] = 0x00000000
+    BaseSoC.mem_map["rom"] = 0x20000000
+
+    def __init__(self, platform, **kwargs):
+        BaseSoC.__init__(self, platform, csr_data_width=32, **kwargs)
+
+        # pcie phy
+        self.submodules.pcie_phy = S7PCIEPHY(platform, platform.request("pcie_x1"))
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.pcie_phy.cd_pcie.clk)
+
+        # pcie endpoint
+        self.submodules.pcie_endpoint = LitePCIeEndpoint(self.pcie_phy, with_reordering=True)
+
+        # pcie wishbone bridge
+        self.submodules.pcie_wishbone = LitePCIeWishboneBridge(self.pcie_endpoint, lambda a: 1)
+        self.add_wb_master(self.pcie_wishbone.wishbone)
+
+        # pcie dma
+        self.submodules.dma = LitePCIeDMA(self.pcie_phy, self.pcie_endpoint, with_loopback=True)
+        self.dma.source.connect(self.dma.sink)
+
+        # pcie msi
+        self.submodules.msi = LitePCIeMSI()
+        self.comb += self.msi.source.connect(self.pcie_phy.msi)
+        self.interrupts = {
+            "dma_writer":    self.dma.writer.irq,
+            "dma_reader":    self.dma.reader.irq
+        }
+        for k, v in sorted(self.interrupts.items()):
+            self.comb += self.msi.irqs[self.interrupt_map[k]].eq(v)
+
+        # pcie led
+        pcie_counter = Signal(32)
+        self.sync.pcie += pcie_counter.eq(pcie_counter + 1)
+        self.comb += self.pcie_led.eq(pcie_counter[26])
+
+
 def main():
     platform = Platform()
     if len(sys.argv) < 2:
-        print("missing target (base)")
+        print("missing target (base or pcie)")
         exit()
     if sys.argv[1] == "base":
         soc = BaseSoC(platform)
+    elif sys.argv[1] == "pcie":
+        soc = PCIeSoC(platform)
     builder = Builder(soc, output_dir="build")
     vns = builder.build()
 
