2769668a0610b977e010075986a499d5764022da
Florent Kermarrec <florent@enjoy-digital.fr>
2018-01-31 20:53:03 +0100

    firmware/dump: cleanup

--- firmware/dump.h
+++ firmware/dump.h
@@ -1,5 +1,9 @@
+#ifndef __DUMP_H
+#define __DUMP_H
+
 void dump_mem(unsigned int addr, unsigned int len);
 void poke_mem(unsigned int addr, unsigned int data);
 void help_memread(void);
 void help_memwrite(void);
 
+#endif
