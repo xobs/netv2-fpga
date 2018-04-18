a7c40cf15877680b3a6baa3a973f7f2aec5ff724
bunnie <bunnie@kosagi.com>
2018-04-18 18:41:32 +0800

    modify default rectangle behavior to be full screen

--- firmware/ci.c
+++ firmware/ci.c
@@ -811,10 +811,10 @@ void ci_service(void)
 		  
 		  hdmi_core_out0_initiator_length_write(m->h_active*m->v_active*4);
 
-		  rectangle_hrect_start_write(0);
-		  rectangle_hrect_end_write(1200);
-		  rectangle_vrect_start_write(0);
-		  rectangle_vrect_end_write(540);
+		  rectangle_hrect_start_write(1);
+		  rectangle_hrect_end_write(1915);
+		  rectangle_vrect_start_write(1);
+		  rectangle_vrect_end_write(1070);
 
 		  wprintf("out hres %d, hscan %d\r\n", hdmi_core_out0_initiator_hres_read(), hdmi_core_out0_initiator_hscan_read());
 		  wprintf("out vres %d, vscan %d\r\n", hdmi_core_out0_initiator_vres_read(), hdmi_core_out0_initiator_vscan_read());
