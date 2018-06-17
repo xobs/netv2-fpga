827994f0ebe149bea258e197d60817d6cf9cc704
Sean Cross <sean@xobs.io>
2018-06-17 20:46:34 +0800

    build: debugging on windows
    Signed-off-by: Sean Cross <sean@xobs.io>

--- build.py
+++ build.py
@@ -2,13 +2,12 @@
 
 import sys
 import os
-
+import subprocess
 
 if sys.version_info[:3] < (3, 5):
     raise SystemExit("You need Python 3.5+")
 
 script_path = os.path.dirname(os.path.realpath(__file__)) + os.path.sep
-print("Script path: " + script_path)
 
 os.environ["PYTHONHASHSEED"] = "1"
 
@@ -16,15 +15,17 @@ python_path = []
 for dep in ["litescope", "liteeth", "litevideo", "litedram", "litex", "migen", "pyserial"]:
     python_path.append(script_path + "deps" + os.path.sep + dep)
 os.environ["PYTHONPATH"] = os.pathsep.join(python_path)
+#os.environ["V"] = "1" # Use to debug Makefiles
+os.environ["PYTHON"] = sys.executable
 
 # Build the bitstream
-os.execl(sys.executable, sys.executable, script_path + os.path.sep + "netv2mvp.py", "video_overlay")
+#subprocess.Popen([sys.executable, script_path + os.path.sep + "netv2mvp.py", "video_overlay"]).wait()
 
 # Start the litex_server script
-#os.execl(sys.executable, sys.executable, "-mlitex.soc.tools.remote.litex_server", "udp")
+#subprocess.Popen([sys.executable, "-mlitex.soc.tools.remote.litex_server"] +  + sys.argv[1:]).wait()
 
 # Start litex_term
-#os.execl(sys.executable, sys.executable, "-mlitex.soc.tools.litex_term", "udp")
+subprocess.Popen([sys.executable, "-mlitex.soc.tools.litex_term"] + sys.argv[1:]).wait()
 
 # Debug environment using bash
 #os.execl("/bin/bash", "bash")
