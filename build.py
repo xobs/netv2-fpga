c8f48ee2316cf11811883c3fa847e2802afbcf52
Sean Cross <sean@xobs.io>
2018-06-18 18:34:59 +0800

    build: support also running litex_server and litex_term
    Add two new hooks to enable running `./build.py litex_term` and
    `./build.py litex_server`.  This will allow the addition of external
    scripts to enable running these two programs.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# This script enables easy, cross-platform building
+
 import sys
 import os
 import subprocess
@@ -19,13 +21,18 @@ os.environ["PYTHONPATH"] = os.pathsep.join(python_path)
 os.environ["PYTHON"] = sys.executable
 
 # Build the bitstream
-subprocess.Popen([sys.executable, script_path + os.path.sep + "netv2mvp.py", "video_overlay"]).wait()
 
 # Start the litex_server script
-#subprocess.Popen([sys.executable, "-mlitex.soc.tools.remote.litex_server"] +  + sys.argv[1:]).wait()
-
-# Start litex_term
-#subprocess.Popen([sys.executable, "-mlitex.soc.tools.litex_term"] + sys.argv[1:]).wait()
+if len(sys.argv) > 1 and sys.argv[1] == "litex_server":
+    subprocess.Popen([sys.executable, "-mlitex.soc.tools.remote.litex_server"] + sys.argv[2:]).wait()
+if len(sys.argv) > 1 and sys.argv[1] == "litex_term":
+    subprocess.Popen([sys.executable, "-mlitex.soc.tools.litex_term"] + sys.argv[2:]).wait()
+else:
+    if len(sys.argv) > 1:
+        args = sys.argv[1:]
+    else:
+        args = ["video_overlay"]
+    subprocess.Popen([sys.executable, script_path + os.path.sep + "netv2mvp.py"] + args).wait()
 
 # Debug environment using bash
 #os.execl("/bin/bash", "bash")
